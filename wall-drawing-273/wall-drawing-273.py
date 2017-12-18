53a9f57d1e805ce167612580d8b2ec0a891a8e2f
Josh Shi <thehandsomepanther@gmail.com>
2017-12-18 13:05:21 -0500

    moving lines

--- wall-drawing-273/wall-drawing-273.py
+++ wall-drawing-273/wall-drawing-273.py
@@ -3,18 +3,17 @@ import random
 import math
 
 DIMENSION = 1000
-NUM_LINES_PER_POINT = 10
 LINE_MIN_LENGTH = 300
 LINE_MAX_LENGTH = 1000
-
+FRAMES = 50
 ANGLE_VARIANCE = .1
-
-size(DIMENSION, DIMENSION)
+LINES = []
 
 # corners
 MIN_ANGLE = 0
 MAX_ANGLE = math.pi/2
-stroke(74/255, 131/255, 178/255, 1)
+NUM_LINES_PER_POINT = 10
+COLOR_BLUE = (74/255, 131/255, 178/255, 1)
 for point in list(itertools.product([0, DIMENSION], repeat=2)):
     for i in range(NUM_LINES_PER_POINT):
         angle = (MAX_ANGLE - MIN_ANGLE) * i/NUM_LINES_PER_POINT + MIN_ANGLE + random.uniform(0, ANGLE_VARIANCE)
@@ -26,19 +25,19 @@ for point in list(itertools.product([0, DIMENSION], repeat=2)):
             angle -= math.pi
         elif point[0] == DIMENSION and point[1] == 0:
             angle += math.pi/2
-            
-        dest = (point[0] + math.cos(angle) * length, point[1] + math.sin(angle) * length)    
         
-        newPath()
-        moveTo(tuple(point))
-        lineTo(dest)
-        closePath()
-        drawPath()
+        LINES.append({
+            'origin': tuple(point),
+            'angle': angle,
+            'length': length,
+            'color': COLOR_BLUE,
+            'offset': random.uniform(0, FRAMES)
+        })
 
 # sides
 MIN_ANGLE = 0
 MAX_ANGLE = math.pi
-stroke(184/255, 103/255, 113/255, 1)
+COLOR_RED = (184/255, 103/255, 113/255, 1)
 for point in [[DIMENSION/2, 0], [0, DIMENSION/2], [DIMENSION, DIMENSION/2], [DIMENSION/2, DIMENSION]]:
     for i in range(NUM_LINES_PER_POINT):
         angle = (MAX_ANGLE - MIN_ANGLE) * i/NUM_LINES_PER_POINT + MIN_ANGLE + random.uniform(0, ANGLE_VARIANCE)
@@ -51,27 +50,54 @@ for point in [[DIMENSION/2, 0], [0, DIMENSION/2], [DIMENSION, DIMENSION/2], [DIM
         elif point[0] == DIMENSION/2 and point[1] == DIMENSION:
             angle += math.pi
             
-        dest = (point[0] + math.cos(angle) * length, point[1] + math.sin(angle) * length)    
-        
-        newPath()
-        moveTo(tuple(point))
-        lineTo(dest)
-        closePath()
-        drawPath()
+        LINES.append({
+            'origin': tuple(point),
+            'angle': angle,
+            'length': length,
+            'color': COLOR_RED,
+            'offset': random.uniform(0, FRAMES)
+        })
 
 # center
 MAX_ANGLE = 0
 MIN_ANGLE = math.pi * 2
-stroke(249/255, 227/255, 8/255, 1)
+COLOR_YELLOW = (249/255, 227/255, 8/255, 1)
 NUM_LINES_PER_POINT = 20
 point = [DIMENSION/2, DIMENSION/2]
 for i in range(NUM_LINES_PER_POINT):
     angle = (MAX_ANGLE - MIN_ANGLE) * i/NUM_LINES_PER_POINT + MIN_ANGLE + random.uniform(0, ANGLE_VARIANCE)
     length = random.uniform(LINE_MIN_LENGTH, LINE_MAX_LENGTH)
-    dest = (point[0] + math.cos(angle) * length, point[1] + math.sin(angle) * length)    
-        
-    newPath()
-    moveTo(tuple(point))
-    lineTo(dest)
-    closePath()
-    drawPath()
+    
+    LINES.append({
+        'origin': tuple(point),
+        'angle': angle,
+        'length': length,
+        'color': COLOR_YELLOW,
+        'offset': random.uniform(0, FRAMES)
+    })
+
+pluck = lambda dict, *args: (dict[arg] for arg in args)
+LENGTH_VARIANCE = 50
+COLOR_WHITE = (240/255, 240/255, 240/255)
+for frame in range(FRAMES):
+    newPage(DIMENSION, DIMENSION)
+    fill(*COLOR_WHITE)
+    rect(0, 0, width(), height())
+    frameDuration(1/20)
+    save()
+    for line in LINES:
+        origin, angle, length, color, offset = pluck(line, 'origin', 'angle', 'length', 'color', 'offset')
+        stroke(*color)
+        theta = (frame/FRAMES + offset) * math.pi * 2
+
+        length += math.cos(theta) * LENGTH_VARIANCE
+
+        newPath()
+        moveTo(origin)
+        dest = (origin[0] + math.cos(angle) * length, origin[1] + math.sin(angle) * length)    
+        lineTo(dest)
+        closePath()
+        drawPath()
+    restore()
+    
+saveImage('wall-drawing-273.gif')
